519bc2d2770e6f083785d59b76ab99baec90cd05
gabriel.tudorica@ymail.com <gabriel.tudorica@ymail.com>
2015-05-02 20:32:03 +0300

    First commit.

---
+++ Session 01 -  RandomPancakeStackDiv2/RandomPancakeStack/RandomPancakeStack/RandomPancakeStackDiv2.cs
@@ -0,0 +1,17 @@
+﻿namespace RandomPancakeStack
+{
+    //Problem: https://arena.topcoder.com/#/u/practiceCode/16444/47662/13749/2/326037
+
+    public class RandomPancakeStackDiv2
+    {       
+        public double ExpectedDeliciousness(int[] d)
+        {
+            if (d.Length == 0)
+            {
+                return 0;
+            }
+
+            return 1;
+        }       
+    }
+}
---
+++ Session 01 -  RandomPancakeStackDiv2/RandomPancakeStack/RandomPancakeStackTests/RandomPancakeStackDiv2Tests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomPancakeStack;
+
+namespace RandomPancakeStackTests
+{
+    [TestClass]
+    public class RandomPancakeStackDiv2Tests
+    {
+        private readonly RandomPancakeStackDiv2 randomPancakeStack = new RandomPancakeStackDiv2();
+
+        [TestMethod]
+        public void HavingZeroPancakes_ExpectedDeliciousnessReturnsZero()
+        {
+            double deliciousness = randomPancakeStack.ExpectedDeliciousness(new int[0]);
+
+            Assert.AreEqual(0, deliciousness);
+        }
+
+        [TestMethod]
+        public void HavingOnePancake_ExpectedDeliciousnessReturnsTheDeliciousnessOfThePancake()
+        {
+            double deliciousness = randomPancakeStack.ExpectedDeliciousness(new[] {1});
+
+            Assert.AreEqual(1, deliciousness);
+        }
+    }
+}
