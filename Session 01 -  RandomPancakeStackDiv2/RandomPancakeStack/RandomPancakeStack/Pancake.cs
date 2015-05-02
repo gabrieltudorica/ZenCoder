d4577682d2194c9a8c8a6c4a82a414cab3d35926
gabriel.tudorica@ymail.com <gabriel.tudorica@ymail.com>
2015-05-02 20:49:31 +0300

    Added Pancake class

---
+++ Session 01 -  RandomPancakeStackDiv2/RandomPancakeStack/RandomPancakeStack/Pancake.cs
@@ -0,0 +1,24 @@
+﻿namespace RandomPancakeStack
+{
+    public class Pancake
+    {
+        private readonly int width;
+        private readonly int deliciousness;
+        
+        public Pancake(int width, int deliciousness)
+        {
+            this.width = width;
+            this.deliciousness = deliciousness;
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public int GetDeliciousness()
+        {
+            return deliciousness;
+        }
+    }
+}
--- Session 01 -  RandomPancakeStackDiv2/RandomPancakeStack/RandomPancakeStack/RandomPancakeStackDiv2.cs
+++ Session 01 -  RandomPancakeStackDiv2/RandomPancakeStack/RandomPancakeStack/RandomPancakeStackDiv2.cs
@@ -1,9 +1,13 @@
-﻿namespace RandomPancakeStack
+﻿using System.Collections.Generic;
+
+namespace RandomPancakeStack
 {
     //Problem: https://arena.topcoder.com/#/u/practiceCode/16444/47662/13749/2/326037
 
     public class RandomPancakeStackDiv2
     {       
+        Dictionary<int, Pancake> pancakes = new Dictionary<int, Pancake>();
+ 
         public double ExpectedDeliciousness(int[] d)
         {
             if (d.Length == 0)
@@ -11,7 +15,25 @@
                 return 0;
             }
 
+            CreatePancakes(d);                   
+
             return 1;
-        }       
+        }
+
+        private void CreatePancakes(int[] deliciousness)
+        {
+            for (int index = 0; index < deliciousness.Length; index++)
+            {
+                CreateSinglePancake(index, deliciousness[index]);
+            }
+        }
+
+        private void CreateSinglePancake(int index, int deliciousness)
+        {
+            int width = index + 1;
+            var pancake = new Pancake(width, deliciousness);
+
+            pancakes.Add(index, pancake);
+        }
     }
 }
