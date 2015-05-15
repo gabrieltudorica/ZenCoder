d409332b338be78b2a2d7dc31866632e52bfa6e4
gabriel.tudorica@ymail.com <gabriel.tudorica@ymail.com>
2015-05-16 00:09:29 +0300

    Added logic for the start of the game

---
+++ Session 02 - Hangman/HangmanGame/HangmanGameTests/HangmanGameStartTests.cs
@@ -0,0 +1,73 @@
+﻿using HangmanGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HangmanGameTests
+{
+    [TestClass]
+    public class HangmanGameStartTests
+    {
+        [TestMethod]
+        public void RemainingAttepts_ReturnsSix()
+        {
+            var hangman = new Hangman("someWord");
+            int remainingAttempts = hangman.RemainingAttempts;
+
+            Assert.AreEqual(6, remainingAttempts);
+        }
+
+        [TestMethod]
+        public void WhenFirstAndLastLettersNotContainedInTheWord_GetPartialWord_RevealsFirstAndLastLetters()
+        {
+            const string inputWord = "someWord";
+
+            var hangman = new Hangman(inputWord);
+            string partialWord = hangman.GetPartialWord();
+
+            Assert.AreEqual("S______D", partialWord);
+        }
+
+        [TestMethod]
+        public void WhenFirstLetterIsDuplicatedInTheWord_GetPartialWord_RevealsTheDuplicatedFirstLetter()
+        {
+            const string inputWord = "bomberman";
+
+            var hangman = new Hangman(inputWord);
+            string partialWord = hangman.GetPartialWord();
+
+            Assert.AreEqual("B__B____N", partialWord);
+        }
+
+        [TestMethod]
+        public void WhenLastLetterIsDuplicatedInTheWord_GetPartialWord_RevelasTheDuplicatedLastLetter()
+        {
+            const string inputWord = "johann";
+
+            var hangman = new Hangman(inputWord);
+            string partialWord = hangman.GetPartialWord();
+
+            Assert.AreEqual("J___NN", partialWord);
+        }
+
+        [TestMethod]
+        public void WhenBothFirstAndLastLettersAreDuplicatedInTheWord_GetPartialWord_RevealsAllDuplicatedLetters()
+        {
+            const string inputWord = "perspective";
+
+            var hangman = new Hangman(inputWord);
+            string partialWord = hangman.GetPartialWord();
+
+            Assert.AreEqual("PE__PE____E", partialWord);
+        }
+
+        [TestMethod]
+        public void WhenFirstAndLastLettersAreTheSame_GetPartialWord_RevealsDuplicatedLetters()
+        {
+            const string inputWord = "synopsis";
+
+            var hangman = new Hangman(inputWord);
+            string partialWord = hangman.GetPartialWord();
+
+            Assert.AreEqual("S____S_S", partialWord);
+        }
+    }
+}
