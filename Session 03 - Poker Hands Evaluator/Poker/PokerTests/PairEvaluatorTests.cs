dbb6a7a97af25caec4dee7f2b241f7c91ead0afe
gabriel.tudorica@ymail.com <gabriel.tudorica@ymail.com>
2015-06-01 23:56:31 +0300

    [Refactoring] Forgot to add this in previous checkin

---
+++ Session 03 - Poker Hands Evaluator/Poker/PokerTests/PairEvaluatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Poker;
+using Poker.Model;
+
+namespace PokerTests
+{
+    [TestClass]
+    public class PairEvaluatorTests
+    {
+        [TestMethod]
+        public void GetRankCategory_ReturnsNone_WhenNoPairsExist()
+        {
+            var pairEvaluator = new PairEvaluator(Dealer.GetCardsForHighCardRankCategory());
+            
+            Assert.AreEqual(RankCategory.None, pairEvaluator.GetRankCategory());
+        }
+
+        [TestMethod]
+        public void GetKeyCards_ReturnsEmptyList_WhenNoPairsExist()
+        {
+            var pairEvaluator = new PairEvaluator(Dealer.GetCardsForHighCardRankCategory());
+
+            Assert.AreEqual(0, pairEvaluator.GetKeyCards().Count);
+        }
+
+        [TestMethod]
+        public void GetRankCategory_ReturnsOnePair_WhenOnePairExists()
+        {
+            var pairEvaluator = new PairEvaluator(Dealer.GetCardsForOnePairRankCategory());
+
+            Assert.AreEqual(RankCategory.OnePair, pairEvaluator.GetRankCategory());
+        }
+
+        [TestMethod]
+        public void GetKeyCards_ReturnsListWithPairCard_WhenOnePairExists()
+        {
+            var pairEvaluator = new PairEvaluator(Dealer.GetCardsForOnePairRankCategory());
+
+            List<Rank> keyCards = pairEvaluator.GetKeyCards();
+            Assert.AreEqual(1, keyCards.Count);
+            Assert.AreEqual(Rank.King, keyCards[0]);
+        }
+    }
+}
