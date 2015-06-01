df97708561950670f94af937c8f930e987e8d4bd
gabriel.tudorica@ymail.com <gabriel.tudorica@ymail.com>
2015-06-01 16:28:24 +0300

    Throw exception when number of cards is different than 5

---
+++ Session 03 - Poker Hands Evaluator/Poker/Poker/PokerHand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Poker
+{
+    public class PokerHand
+    {
+        private readonly object[] cards;
+
+        public PokerHand(object[] cards)
+        {
+            this.cards = cards;
+        }
+
+        public void GetRank()
+        {
+            if (cards.Length != 5)
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
---
+++ Session 03 - Poker Hands Evaluator/Poker/PokerTests/PokerHandTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Poker;
+
+namespace PokerTests
+{
+    [TestClass]
+    public class PokerHandTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetRank_ThrowsException_WhenNumberOfCardsIsDifferentThanFive()
+        {
+            var pokerHand = new PokerHand(new object[] {});
+            pokerHand.GetRank();
+        }
+    }
+}
