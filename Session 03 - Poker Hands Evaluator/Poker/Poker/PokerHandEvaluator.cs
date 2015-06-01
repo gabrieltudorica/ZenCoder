889534b0ad4ce8bc697502ec1b97e9133d7c7c6a
gabriel.tudorica@ymail.com <gabriel.tudorica@ymail.com>
2015-06-01 20:17:08 +0300

    Forgot to check in one item

---
+++ Session 03 - Poker Hands Evaluator/Poker/Poker/PokerHandEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Poker
+{
+    public class PokerHandEvaluator
+    {
+        private const int RequiredNumberOfCards = 5;
+
+        private readonly object[] cards;
+
+        public PokerHandEvaluator(object[] cards)
+        {
+            this.cards = cards;
+
+            if (cards.Length != RequiredNumberOfCards)
+            {
+                throw new ArgumentException();
+            }
+        }
+         
+        public Rank GetRank()
+        {           
+            return Rank.HighCard;
+        }
+    }
+}
