c751df25738c6c1a9ca8658b0a76b7f9f7313cfa
gabriel.tudorica@ymail.com <gabriel.tudorica@ymail.com>
2015-06-05 01:54:36 +0300

    Added tests for GetRankCategory and GetHighCards for two pairs

--- Session 03 - Poker Hands Evaluator/Poker/PokerTests/EvaluatorsTests/PairEvaluatorTests.cs
+++ Session 03 - Poker Hands Evaluator/Poker/PokerTests/EvaluatorsTests/PairEvaluatorTests.cs
@@ -8,11 +8,7 @@ namespace PokerTests.EvaluatorsTests
 {
     [TestFixture]
     public class PairEvaluatorTests
-    {
-        private const Rank Pair = Rank.Ace;
-        private static readonly Rank[] OnePairHighCards = {Rank.Two, Rank.Three, Rank.Five};        
-        private readonly List<Card> onePair = Dealer.DealOnePairThreeHighCards(Pair, OnePairHighCards);
-
+    {              
         private readonly List<Card> highCards = Dealer.DealStrongHighCard();
 
         [Test]
@@ -31,10 +27,14 @@ namespace PokerTests.EvaluatorsTests
             Assert.AreEqual(5, pairEvaluator.GetHighCardsDescending().Count);
         }
 
+        private const Rank PairOne = Rank.Ace;
+        private static readonly Rank[] OnePairHighCards = { Rank.Two, Rank.Three, Rank.Five };
+        private readonly List<Card> onePairCards = Dealer.DealOnePairThreeHighCards(PairOne, OnePairHighCards);
+
         [Test]
         public void GetRankCategory_ReturnsOnePair_WhenOnePairExists()
         {
-            var pairEvaluator = new PairEvaluator(onePair);
+            var pairEvaluator = new PairEvaluator(onePairCards);
 
             Assert.AreEqual(RankCategory.OnePair, pairEvaluator.GetRankCategory());
         }
@@ -42,15 +42,40 @@ namespace PokerTests.EvaluatorsTests
         [Test]
         public void GetHighCardsDescending_ReturnsListWithPairCardAndHighCardsInDescendingOrder_WhenOnePairExists()
         {
-            var pairEvaluator = new PairEvaluator(onePair);
+            var pairEvaluator = new PairEvaluator(onePairCards);
 
             List<Rank> keyCards = pairEvaluator.GetHighCardsDescending();
             
             Assert.AreEqual(4, keyCards.Count);
-            Assert.AreEqual(Pair, keyCards[0]);
+            Assert.AreEqual(PairOne, keyCards[0]);
             Assert.AreEqual(OnePairHighCards[2], keyCards[1]);
             Assert.AreEqual(OnePairHighCards[1], keyCards[2]);
             Assert.AreEqual(OnePairHighCards[0], keyCards[3]);
+        }
+
+        private const Rank PairTwo = Rank.King;
+        private const Rank TwoPairsHighCard = Rank.Two;
+        private readonly List<Card> twoPairsCards = Dealer.DealTwoPairsOneHighCard(PairOne, PairTwo, TwoPairsHighCard);
+
+        [Test]
+        public void GetRankCategory_ReturnsTwoPairs_WhenTwoPairsExist()
+        {
+            var pairEvaluator = new PairEvaluator(twoPairsCards);
+
+            Assert.AreEqual(RankCategory.TwoPairs, pairEvaluator.GetRankCategory());
+        }
+
+        [Test]
+        public void GetHighCardsDescending_ReturnsOneHighCard_WhenTwoPairsExist()
+        {
+            var pairEvaluator = new PairEvaluator(twoPairsCards);
+
+            List<Rank> highCard = pairEvaluator.GetHighCardsDescending();
+
+            Assert.AreEqual(3, highCard.Count);
+            Assert.AreEqual(PairOne, highCard[0]);
+            Assert.AreEqual(PairTwo, highCard[1]);
+            Assert.AreEqual(TwoPairsHighCard, highCard[2]);
         }
     }
 }
