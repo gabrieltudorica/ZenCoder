c51dc023ceb8bb277a19de68a365cd499c7377db
gabriel.tudorica@ymail.com <gabriel.tudorica@ymail.com>
2015-06-07 01:35:33 +0300

    Added rest of three of a kind tests

--- Session 03 - Poker Hands Evaluator/Poker/PokerTests/PokerHandTests/ThreeOfAKindTests.cs
+++ Session 03 - Poker Hands Evaluator/Poker/PokerTests/PokerHandTests/ThreeOfAKindTests.cs
@@ -1,14 +1,26 @@
 ﻿using NUnit.Framework;
+using Poker;
+using Poker.Model;
 
 namespace PokerTests.PokerHandTests
 {
     [TestFixture]
     public class ThreeOfAKindTests
     {
+        private static readonly Rank[] HighCards = { Rank.Two, Rank.Three };
+        private readonly PokerHand strongThreeOfAKind = new PokerHand(Dealer.DealThreeOfAKindTwoHighCards(Rank.Ace, HighCards));
+        private readonly PokerHand weakThreeOfAKind = new PokerHand(Dealer.DealThreeOfAKindTwoHighCards(Rank.King, HighCards));
+
         [Test]
         public void StrongThreeOfAKind_ComparedWith_WeakThreeOfAKind_ReturnsStrong()
-        {
+        {          
+            Assert.AreEqual(Strength.Strong, strongThreeOfAKind.CompareWith(weakThreeOfAKind));
+        }
 
+        [Test]
+        public void WeakThreeOfAKind_ComparedWith_StrongThreeOfAKind_ReturnsWeak()
+        {
+            Assert.AreEqual(Strength.Weak, weakThreeOfAKind.CompareWith(strongThreeOfAKind));
         }
     }
 }
